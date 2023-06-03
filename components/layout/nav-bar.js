05064a3c3e1e13710b86e395c3235f2fa65cffcd
Arinjay Singh <arinjay.singh@icloud.com>
2023-06-03 00:25:46 -0400

    added dropdown arrows to each of the nav items

--- components/layout/nav-bar.js
+++ components/layout/nav-bar.js
@@ -3,6 +3,7 @@ import PersonalLogo from "./personal-logo";
 import classes from "./nav-bar.module.css";
 import GithubLogo from "./github-logo";
 import LinkedinLogo from "./linkedin-logo";
+import DropArrrow from "./drop-arrow";
 
 function NavigationBar() {
   return (
@@ -13,16 +14,28 @@ function NavigationBar() {
       <nav>
         <ul>
           <li>
-            <Link href="/about">About</Link>
+            <Link href="/about" className={classes.navitem}>
+              About
+              <DropArrrow />
+            </Link>
           </li>
           <li>
-            <Link href="/skills">Skills</Link>
+            <Link href="/skills" className={classes.navitem}>
+              Skills
+              <DropArrrow />
+            </Link>
           </li>
           <li>
-            <Link href="/experience">Experience</Link>
+            <Link href="/experience" className={classes.navitem}>
+              Experience
+              <DropArrrow />
+            </Link>
           </li>
           <li>
-            <Link href="/projects">Projects</Link>
+            <Link href="/projects" className={classes.navitem}>
+              Projects
+              <DropArrrow />
+            </Link>
           </li>
           <li className={classes.vertical}></li>
           <li className={classes.logo}>
@@ -33,7 +46,9 @@ function NavigationBar() {
           </li>
           <li className={classes.vertical}></li>
           <li>
-            <Link href="/resume" className={classes.button}>Resume</Link>
+            <Link href="/resume" className={classes.button}>
+              Resume
+            </Link>
           </li>
         </ul>
       </nav>
