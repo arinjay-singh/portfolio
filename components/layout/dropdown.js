c08d4d6ff6de257e75fcce6158ca95145e100063
Arinjay Singh <arinjay.singh@icloud.com>
2023-06-03 17:16:40 -0400

    finally finished dropdown menu, fully functioning

---
+++ components/layout/dropdown.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import classes from "./nav-bar.module.css";
+
+function Dropdown(props) {
+  const { navitem, items } = props;
+
+  return (
+    <div className={classes.dropdown}>
+      {items.map((item) => (
+        <Link href={`/${navitem}/${item}`}>
+          {item}
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Dropdown;
--- components/layout/nav-bar.js
+++ components/layout/nav-bar.js
@@ -4,6 +4,7 @@ import classes from "./nav-bar.module.css";
 import GithubLogo from "./github-logo";
 import LinkedinLogo from "./linkedin-logo";
 import DropArrrow from "./drop-arrow";
+import Dropdown from "./dropdown";
 
 function NavigationBar() {
   return (
@@ -13,29 +14,49 @@ function NavigationBar() {
       </Link>
       <nav>
         <ul>
-          <li>
+          <li className={classes.drop}>
             <Link href="/about" className={classes.navitem}>
               About
-              <DropArrrow />
+              <DropArrrow className={classes.svg}/>
             </Link>
+            <Dropdown
+              navitem="about"
+              items={["me", "this website"]}
+              className={classes.dropdown}
+            />
           </li>
-          <li>
+          <li className={classes.drop}>
             <Link href="/skills" className={classes.navitem}>
               Skills
               <DropArrrow />
             </Link>
+            <Dropdown
+              navitem="skills"
+              items={["languages", "frameworks", "tools"]}
+              className={classes.dropdown}
+            />
           </li>
-          <li>
+          <li className={classes.drop}>
             <Link href="/experience" className={classes.navitem}>
               Experience
               <DropArrrow />
             </Link>
+            <Dropdown
+              navitem="experience"
+              items={["work", "projects"]}
+              className={classes.dropdown}
+            />
           </li>
-          <li>
+          <li className={classes.drop}>
             <Link href="/projects" className={classes.navitem}>
               Projects
               <DropArrrow />
             </Link>
+            <Dropdown
+              navitem="projects"
+              items={["personal", "school"]}
+              className={classes.dropdown}
+            />
           </li>
           <li className={classes.vertical}></li>
           <li className={classes.logo}>
