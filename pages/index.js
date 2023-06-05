b52b40196f1722fee79fc9e3d34f199393bf470f
Arinjay Singh <arinjay.singh@icloud.com>
2023-06-05 18:33:44 -0400

    added to home page and switched to notion font

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import classes from "./index.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={classes.title}>
+      <h3> Hi, my name is </h3>
+      <h1> Arinjay Singh </h1>
+      <h3> I'm a software and web development enthusiast <br/> studying Computer Science and Economics at Northeastern University. </h3>
     </div>
   );
 }
