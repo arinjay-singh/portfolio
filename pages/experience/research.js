4a174e726a7dc0f9b3ca20a957445b1531a3e25c
Arinjay Singh <arinjay.singh@icloud.com>
2023-06-08 23:02:10 -0400

    added title to research page

--- pages/experience/research.js
+++ pages/experience/research.js
@@ -1,5 +1,11 @@
+import classes from './research.module.css';
+
 function Research() {
-    return <div></div>;
+    return <div className={classes.page}>
+        <div className={classes.title}>
+            <h1>Research</h1>
+        </div>
+    </div>;
 }
 
 export default Research;
