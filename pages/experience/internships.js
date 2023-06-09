59f345777669a7dbbf50a9046921f488832e2e97
Arinjay Singh <arinjay.singh@icloud.com>
2023-06-08 23:02:00 -0400

    added title to internship page

--- pages/experience/internships.js
+++ pages/experience/internships.js
@@ -1,5 +1,11 @@
+import classes from './internships.module.css';
+
 function Internships() {
-    return <div></div>;
+    return <div className={classes.page}>
+        <div className={classes.title}>
+            <h1>Internships</h1>
+        </div>
+    </div>;
 }
 
 export default Internships;
