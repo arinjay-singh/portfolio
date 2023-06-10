9f1738f901c4782a5153af504372e71837aac7af
Arinjay Singh <arinjay.singh@icloud.com>
2023-06-09 22:33:52 -0400

    added title to personal projects page

--- pages/projects/personal.js
+++ pages/projects/personal.js
@@ -1,5 +1,11 @@
+import classes from './personal.module.css';
+
 function PersonalProjects() {
-    return <div></div>;
+    return <div className={classes.page}>
+        <div className={classes.title}>
+            <h1>Personal Projects</h1>
+        </div>
+    </div>;
 }
 
 export default PersonalProjects;
