a0a232b92fb7b4c1cd6fa3c364d8716459ebb936
Arinjay Singh <arinjay.singh@icloud.com>
2023-06-09 22:34:01 -0400

    added title to school projects page

--- pages/projects/school.js
+++ pages/projects/school.js
@@ -1,5 +1,11 @@
+import classes from './projects.module.css';
+
 function SchoolProjects() {
-    return <div></div>;
+    return <div className={classes.page}>
+        <div className={classes.title}>
+            <h1>School Projects</h1>
+        </div>
+    </div>;
 }
 
 export default SchoolProjects;
