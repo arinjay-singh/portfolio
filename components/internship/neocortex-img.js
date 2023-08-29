6a58647b54659499d49ce1718f25923b06fd9cd9
Arinjay Singh <arinjay.singh@icloud.com>
2023-08-29 16:04:44 -0700

    adjusting path

--- components/internship/neocortex-img.js
+++ components/internship/neocortex-img.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 function DataAnalyticsImage() {
   return (
     <Image
-      src="images/neocortex-images/data-analytics-img.jpeg"
+      src="./images/neocortex-images/data-analytics-img.jpeg"
       alt="Data Analytics Logo"
       width={710}
       height={410}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   basePath: '/portfolio',
+  reactStrictMode: false,
 }
 
 module.exports = nextConfig
