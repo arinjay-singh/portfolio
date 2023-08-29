1942d9e5dbd2d25e08b7c43701eb05b65236f172
Arinjay Singh <arinjay.singh@icloud.com>
2023-08-29 16:07:48 -0700

    trying again

--- components/layout/personal-logo.js
+++ components/layout/personal-logo.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 function PersonalLogo() {
   return (
     <Image
-      src="images/personal-logo/arinjay-logo.svg"
+      src="/portfolio/images/personal-logo/arinjay-logo.svg"
       alt="Arinjay Singh Logo"
       width={195}
       height={78}
