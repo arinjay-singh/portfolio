05d11c2a87a2164ed93687d98c3495f2eb49fcf2
Arinjay Singh <arinjay.singh@icloud.com>
2023-08-29 02:21:26 -0700

    take resume off

--- components/home-page/memoji.js
+++ components/home-page/memoji.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Memoji() {
   return (
     <Image
-      src="portfolio/public/images/memoji/memoji.svg"
+      src="/images/memoji/memoji.svg"
       alt="Arinjay Singh Memoji"
       width={300}
       height={300}
